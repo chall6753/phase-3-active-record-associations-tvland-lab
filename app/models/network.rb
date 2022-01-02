bd79d32aeed2d63c7ac709e64ff3e537f9a69323
Charles <chall70101@gmail.com>
2022-01-02 12:26:59 -0700

    commit tree 1fbaf2a9fe9054f1a724bd12feec673ee0d3a35f

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,9 @@
+require "pry"
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+       # binding.pry
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
+require "pry"
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  def actors_list
+    x = []
+    self.actors.each do |actor|
+        x << "#{actor.first_name} #{actor.last_name}"
+    
+    end
+    x
+  end
 end
